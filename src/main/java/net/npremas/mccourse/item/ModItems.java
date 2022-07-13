0eef0d518e0d8e22910839016f508c9426850294
n-premasinghe <99045891+n-premasinghe@users.noreply.github.com>
2022-07-13 16:06:47 -0400

    rarity change

--- src/main/java/net/npremas/mccourse/item/ModItems.java
+++ src/main/java/net/npremas/mccourse/item/ModItems.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
 import net.npremas.mccourse.MCCourseMod;
 
 import net.minecraft.util.registry.Registry;
@@ -11,10 +12,10 @@ import net.minecraft.util.registry.Registry;
 public class ModItems {
 
     public static final Item CHRONOCALCUM_INGOT = registerItem("chronocalcum_ingot",
-            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+            new Item(new FabricItemSettings().rarity(Rarity.RARE).group(ItemGroup.MISC)));
 
     public static final Item RAW_CHRONOCALCUM = registerItem("raw_chronocalcum",
-            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+            new Item(new FabricItemSettings().rarity(Rarity.UNCOMMON).group(ItemGroup.MISC)));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(MCCourseMod.MOD_ID, name), item);
