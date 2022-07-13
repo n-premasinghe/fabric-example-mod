40f244fe215743744745351a85531fbe7ff8cfae
n-premasinghe <99045891+n-premasinghe@users.noreply.github.com>
2022-07-13 15:59:35 -0400

    added chronocalcum

--- src/main/java/net/npremas/mccourse/MCCourseMod.java
+++ src/main/java/net/npremas/mccourse/MCCourseMod.java
@@ -1,6 +1,7 @@
 package net.npremas.mccourse;
 
 import net.fabricmc.api.ModInitializer;
+import net.npremas.mccourse.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,10 +14,7 @@ public class MCCourseMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/npremas/mccourse/item/ModItems.java
@@ -0,0 +1,27 @@
+package net.npremas.mccourse.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.npremas.mccourse.MCCourseMod;
+
+import net.minecraft.util.registry.Registry;
+
+public class ModItems {
+
+    public static final Item CHRONOCALCUM_INGOT = registerItem("chronocalcum_ingot",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    public static final Item RAW_CHRONOCALCUM = registerItem("raw_chronocalcum",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(MCCourseMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        System.out.println("Registering Mod Items for "  + MCCourseMod.MOD_ID);
+    }
+
+}
