996caafa227339efb19a1cb82e7e4de31376e246
n-premasinghe <99045891+n-premasinghe@users.noreply.github.com>
2022-07-15 18:53:44 -0400

    Update ModChronocalcumKatanaItem.java

--- src/main/java/net/npremas/mccourse/item/custom/ModChronocalcumKatanaItem.java
+++ src/main/java/net/npremas/mccourse/item/custom/ModChronocalcumKatanaItem.java
@@ -1,12 +1,20 @@
 package net.npremas.mccourse.item.custom;
 
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.effect.StatusEffect;
-import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.effect.StatusEffects;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.StackReference;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
 import net.minecraft.item.ToolMaterial;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.screen.slot.Slot;
+import net.minecraft.util.ClickType;
+
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
 
 public class ModChronocalcumKatanaItem extends SwordItem {
     public ModChronocalcumKatanaItem(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Settings settings) {
@@ -16,8 +24,17 @@ public class ModChronocalcumKatanaItem extends SwordItem {
     @Override
     public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
 
-        target.addStatusEffect(new StatusEffectInstance(StatusEffects.INSTANT_DAMAGE, 200, 2), attacker);
-
+//        target.addStatusEffect(new StatusEffectInstance(StatusEffects.INSTANT_DAMAGE, 200, 2), attacker);
         return super.postHit(stack, target, attacker);
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand) {
+        Vec3d aim = player.getRotationVector();
+        if (world.isClient()) {
+            world.addParticle(ParticleTypes.DAMAGE_INDICATOR, player.getX(),player.getY(), player.getZ(), aim.x, aim.y, aim.z);
+            System.out.println("Right Clicked!");
+        }
+        return super.use(world, player, hand);
     }
 }
