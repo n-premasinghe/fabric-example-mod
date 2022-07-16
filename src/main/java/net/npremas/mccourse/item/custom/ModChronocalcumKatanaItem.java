4fe9fb53f391c50268cc1db2d4b7df36529cbd7e
n-premasinghe <99045891+n-premasinghe@users.noreply.github.com>
2022-07-15 23:24:25 -0400

    Update ModChronocalcumKatanaItem.java

--- src/main/java/net/npremas/mccourse/item/custom/ModChronocalcumKatanaItem.java
+++ src/main/java/net/npremas/mccourse/item/custom/ModChronocalcumKatanaItem.java
@@ -13,6 +13,7 @@ import net.minecraft.util.ClickType;
 
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.math.Position;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
@@ -31,9 +32,19 @@ public class ModChronocalcumKatanaItem extends SwordItem {
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand) {
         Vec3d aim = player.getRotationVector();
+
+        player.getItemCooldownManager().set(this, 7);
+        
+
         if (world.isClient()) {
-            world.addParticle(ParticleTypes.DAMAGE_INDICATOR, player.getX(),player.getY(), player.getZ(), aim.x, aim.y, aim.z);
-            System.out.println("Right Clicked!");
+
+            for (float i = -2; i < 2; i+= 0.05) {
+                world.addParticle(ParticleTypes.DAMAGE_INDICATOR, player.getX(), player.getY() + 0.5D, player.getZ(), aim.x * 10 + i, aim.y * 10 + i, aim.z * 10 + i);
+            }
+//
+//            for (float i = 2; i < -2; i-= 0.05) {
+//                world.addParticle(ParticleTypes.DAMAGE_INDICATOR, player.getX(), player.getY() + 0.5D, player.getZ(), aim.x * 10 - i, aim.y * 10 - i, aim.z * 10 - i);
+//            }
         }
         return super.use(world, player, hand);
     }
